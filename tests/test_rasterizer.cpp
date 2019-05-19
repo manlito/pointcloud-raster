db3e96063b37f90201715e77cdb969a82b6f730b
Manlio Barajas <manlito@gmail.com>
2019-05-18 21:29:33 -0500

    Fix build when LAS and PNG support are both off

--- tests/test_rasterizer.cpp
+++ tests/test_rasterizer.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <pointcloud_raster/raster/pointcloud_rasterizer.hpp>
-#include <pointcloud_raster/io/las/las_reader.hpp>
+#include <pointcloud_raster/io/txt/txt_reader.hpp>
 #include <gtest/gtest.h>
 
 using namespace pointcloud_raster;
@@ -12,9 +12,11 @@ TEST(Raster, Rasterize)
     rasterizerOptions.rasterSize.height = 32;
     raster::PointcloudRasterizer rasterizer;
     rasterizer.AddOutputRaster(rasterizerOptions);
-    rasterizer.AddInputProvider(new io::LASReader(LAS_FILE_SIMPLE_INPUT));
+    rasterizer.AddInputProvider(new io::TXTReader(TXT_FILE_SIMPLE_INPUT));
     rasterizer.Rasterize();
     const auto &image = rasterizer.GetRasterImages().front();
+    EXPECT_EQ(image.Width(), 32);
+    EXPECT_EQ(image.Height(), 32);
 #ifdef POINTCLOUD_RASTER_PNG_SUPPORT
     std::string outputImage = std::string(TESTS_OUTPUT_DIR) + "/rasterized_out.png";
     image.SaveAsPNG(outputImage);
