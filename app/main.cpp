d971ab0df22cc1ff034979655d15574e235b6da3
Manlio Barajas <manlito@gmail.com>
2019-05-18 20:48:23 -0500

    Improve app interface to allow specific viewpoint

--- app/main.cpp
+++ app/main.cpp
@@ -11,23 +11,32 @@ main(int argc, char *argv[])
         std::cout << "Usage: ./pointcloud_raster_app input_cloud format output_dir" << std::endl;
         std::cout << " - input cloud is the path to the file to raster" << std::endl;
         std::cout << " - format can be TXT or LAS" << std::endl;
+        std::cout << " - raster viewpoint, can be TOP, SIDE, FRONT, PERSPECTIVE or ALL" << std::endl;
         std::cout << " - output prefix for results. Parent folder should exist" << std::endl;
         std::cout << " - (optional) max width for raster" << std::endl;
         std::cout << " Example: ./pointcloud_raster_app input.las LAS output/screenshot 2048 " << std::endl;
         return EXIT_FAILURE;
     }
     const std::string pointcloudFile(argv[1]);
-    const std::string pointcloudFormat(argv[2]);
-    std::cout << "Rendering pointcloud" << pointcloudFile << " in format " << pointcloudFormat << std::endl;
+    std::string pointcloudFormat(argv[2]);
+    std::transform(pointcloudFormat.begin(), pointcloudFormat.end(), pointcloudFormat.begin(), ::toupper);
+    std::string rasterViewPreset(argv[3]);
+    std::transform(rasterViewPreset.begin(), rasterViewPreset.end(), rasterViewPreset.begin(), ::toupper);
 
-    const std::vector<std::pair<std::string, pointcloud_raster::ViewPointPreset>> viewPresets {
-        {"top", pointcloud_raster::ViewPointPreset::TOP},
-        {"side", pointcloud_raster::ViewPointPreset::SIDE},
-        {"front", pointcloud_raster::ViewPointPreset::FRONT},
-        {"perspective", pointcloud_raster::ViewPointPreset::FRONT_ISOMETRIC}
-    };
+    std::cout << "Rasterizing " << pointcloudFile << " format " << pointcloudFormat << " using viewpoint " << rasterViewPreset << std::endl;
 
-    const int rasterSize = argc >= 5 ? std::stoi(argv[4]) : 1024;
+    std::vector<std::pair<std::string, pointcloud_raster::ViewPointPreset>> viewPresets;
+    if (rasterViewPreset == "TOP" || rasterViewPreset == "ALL")
+        viewPresets.push_back({"top", pointcloud_raster::ViewPointPreset::TOP});
+    if (rasterViewPreset == "SIDE" || rasterViewPreset == "ALL")
+        viewPresets.push_back({"side", pointcloud_raster::ViewPointPreset::SIDE});
+    if (rasterViewPreset == "FRONT" || rasterViewPreset == "ALL")
+        viewPresets.push_back({"front", pointcloud_raster::ViewPointPreset::FRONT});
+    if (rasterViewPreset == "PERSPECTIVE" || rasterViewPreset == "ALL")
+        viewPresets.push_back({"perspective", pointcloud_raster::ViewPointPreset::FRONT_ISOMETRIC});
+
+
+    const int rasterSize = argc >= 6 ? std::stoi(argv[5]) : 1024;
 
     pointcloud_raster::raster::PointcloudRasterizer rasterizer;
     for (const auto &[suffix, viewProfile] : viewPresets)
@@ -39,8 +48,13 @@ main(int argc, char *argv[])
     }
     if (pointcloudFormat == "LAS")
         rasterizer.AddInputProvider(new pointcloud_raster::io::LASReader(pointcloudFile));
-    else
+    else if (pointcloudFormat == "TXT")
         rasterizer.AddInputProvider(new pointcloud_raster::io::TXTReader(pointcloudFile));
+    else
+    {
+        std::cerr << "Unknown format " << pointcloudFormat << std::endl;
+        return EXIT_FAILURE;
+    }
     if (!rasterizer.Rasterize())
     {
         std::cerr << "Rasterization failed" << std::endl;
@@ -61,7 +75,7 @@ main(int argc, char *argv[])
         auto rasterImageIterator = rasterizer.GetRasterImages().begin();
         for (const auto &[suffix, viewProfile] : viewPresets)
         {
-            const std::string pngFile = std::string(argv[3]) + "_" + suffix + ".png";
+            const std::string pngFile = std::string(argv[4]) + "_" + suffix + ".png";
             std::cout << "Saving image " << pngFile << std::endl;
             if (!rasterImageIterator->SaveAsPNG(pngFile))
                 std::cerr << "Error saving image" << std::endl;
